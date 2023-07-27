2b1a04234911d8e0a36c30edd7b4c080ce7d46cd
sensen <2848294993@qq.com>
2023-07-27 19:45:46 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app-container"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.app-container {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
